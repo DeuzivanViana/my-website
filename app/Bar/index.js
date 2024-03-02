c8db7223089c80f1bb3fe7a21532a04a312bf8f4
Deuzivan Lima <deuzivanlimaviana@proton.me>
2024-03-02 11:41:49 -0300

    fix bar progress glitch when percent progress is changed

--- app/Bar/index.js
+++ app/Bar/index.js
@@ -3,7 +3,7 @@ export const Bar = ({ title, progress }) => {
         <>
             <h2 className="text-neutral-400 text-sm ml-4">{ title }</h2>
             <div className="bg-indigo-900 m-4 rounded-md text-sm text-white mt-2">
-                <span className={`bg-indigo-500 p-2 block w-${ progress } rounded-md`}>{ progress }%</span>
+                <span className={`bg-indigo-500 p-2 block rounded-md`} style={{width: `${progress}%`}}>{ progress }%</span>
             </div>
         </>
     )
