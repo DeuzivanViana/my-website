96c53a03fa27bb3f552cf2506127e7b66b0111df
Deuzivan Lima <deuzivanlimaviana@proton.me>
2024-03-02 19:26:48 -0300

    add icon at main naviagtion bar

--- app/components/NavigationBar/index.js
+++ app/components/NavigationBar/index.js
@@ -1,7 +1,13 @@
+import { Menu } from 'lucide-react'
+import Link from 'next/link'
+
 export const NavigationBar = () => {
     return (
-        <nav className="bg-neutral-950 p-4 shadow-md">
-            <h1 className="text-white">Home</h1>
-        </nav>
+        <>
+            <nav className='bg-neutral-950 p-4 shadow-md flex justify-between'>
+                <h1 className='text-white'>Home</h1>
+                <Menu className='text-white'/>
+            </nav>
+        </>
     )
 }
