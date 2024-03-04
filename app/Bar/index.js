347ab5156ee82d18cfd7ef9da91d02060ebc5ec8
Deuzivan Lima <deuzivanlimaviana@proton.me>
2024-03-04 07:53:53 -0300

    update styles and add more information about me and my jobs

--- app/Bar/index.js
+++ app/Bar/index.js
@@ -2,8 +2,8 @@ export const Bar = ({ title, progress }) => {
     return (
         <>
             <h2 className="text-neutral-400 text-sm ml-4">{ title }</h2>
-            <div className="bg-indigo-900 m-4 rounded-md text-sm text-white mt-2">
-                <span className={`bg-indigo-500 p-2 block rounded-md`} style={{width: `${progress}%`}}>{ progress }%</span>
+            <div className="bg-indigo-900 m-4 rounded-full text-sm text-white mt-2">
+                <span className={`bg-indigo-500 p-2 pl-4 block rounded-full`} style={{width: `${progress}%`}}>{ progress }%</span>
             </div>
         </>
     )
--- app/components/Banner/index.js
+++ app/components/Banner/index.js
@@ -2,8 +2,17 @@ export const Banner = () => {
     return (
         <div className="text-white p-4">
             <h1 className="text-3xl">Hey there, i&apos;m <span className="text-indigo-500">Deuzivan</span>!</h1>
-            <p className="text-indigo-500 text-xl py-2">Web Develepor</p>
+            <h2 className="text-indigo-500 text-xl py-2">About Me</h2>
             <p className="text-sm text-neutral-400">I&apos;ve been passionate about programming since I was 14 years old, I&apos;ve been trying my whole life to evolve and learn with each new project.</p>
+            <h2 className="text-indigo-500 text-xl py-2">Languages</h2>
+            <ul>
+                <li className="text-sm">- Portuguese (Native)</li>
+                <li className="text-sm">- English (Basic)</li>
+            </ul>
+            <h2 className="text-indigo-500 text-xl py-2">Jobs History</h2>
+            <ul>
+                <li className="text-sm">- Local micro-business UPINOVA (2022)</li>
+            </ul>
         </div>
     )
 }
