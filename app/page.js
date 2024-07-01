6aed2ff5b0132c5619900476c5c9ccb4124f35ca
DeuzivanLima <deuzivanlimaviana@proton.me>
2024-07-01 18:46:59 -0300

    center text and change engima to init

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 		<main className="flex item-center h-[100vh] bg-white">
-			<h1 className="text-4xl text-center p-4 place-content-center mb-48">O que tu está procura aqui? Calma lá, veja bem, entenda...</h1>
+			<h1 className="text-4xl text-center p-4 place-content-center m-auto">O que tu está procura aqui? Calma lá, veja bem, entenda...</h1>
 		</main>
   );
 }
