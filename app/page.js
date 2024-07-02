61f457e6c67ab9257b9955805aac49f26cbede73
DeuzivanLima <deuzivanlimaviana@proton.me>
2024-07-02 17:02:56 -0300

    fix a miss with my portuguese

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 		<main className="flex item-center h-[100vh] bg-white">
-			<h1 className="text-4xl text-center p-4 mt-[20%] place-content-center m-auto">Está página está em desenvolvimento...</h1>
+			<h1 className="text-4xl text-center p-4 mt-[20%] place-content-center m-auto">Esta página está em desenvolvimento...</h1>
 		</main>
   );
 }
