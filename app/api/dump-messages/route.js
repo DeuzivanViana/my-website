b9809fb693bc4d8d3273ba75d2c01c064bd303d6
DeuzivanLima <deuzivanlimaviana@proton.me>
2024-07-10 14:01:10 -0300

    remove depracted folder

--- app/api/dump-messages/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-import { db } from "@/db"
-
-export const dynamic = "force-dynamic"
-
-export const GET = async (req) => {
-    const queries = await db.message.findMany()
-    return NextResponse.json(queries.reverse())
-}
