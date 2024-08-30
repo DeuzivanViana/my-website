036b234fc0d44033acea47f56b252e0c1972c19d
Deuzivan <158287163+DeuzivanLima@users.noreply.github.com>
2024-08-30 02:14:20 +0000

    Change color paletter and add social medias

--- app/components/Banner/index.js
+++ app/components/Banner/index.js
@@ -1,22 +1,48 @@
-'use client'
-import { motion } from "framer-motion"
-import styles from "./index.module.scss"
+"use client";
+import { motion } from "framer-motion";
+import styles from "./index.module.scss";
+import { Github, Instagram, Linkedin, Youtube } from "lucide-react";
+import Link from "next/link";
 
 export const Banner = () => {
-    return (
-        <motion.div className={`${styles.container} text-white p-4 max-w-[1500px] m-auto`}
-            initial={{opacity: 0, x: -300}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.5}}
-        >
-            <h1 className="text-6xl font-bold">Hey there, i&apos;m <span className="text-[#CC2936]">Deuzivan</span>!</h1>
-            <h2 className="text-white text-4xl py-2 font-bold">About Me</h2>
-            <p className="text-2xl text-neutral-400 font-light">I&apos;ve been passionate about programming since I was 14 years old, I&apos;ve been trying my whole life to evolve and learn with each new project.</p>
-            <h2 className="text-white text-4xl py-2 font-bold">Languages</h2>
-            <ul className="text-neutral-400">
-                <li className="text-1xl font-light">- Portuguese (Native)</li>
-                <li className="text-1xl font-light">- English (Basic)</li>
-            </ul>
-        </motion.div>
-    )
-}
+  return (
+    <motion.div
+      className={`${styles.container} text-white p-4 max-w-[1500px] m-auto`}
+      initial={{ opacity: 0, x: -300 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h1 className="text-5xl font-bold">
+        Hey there, i&apos;m <span className="text-[#2bbdf2]">Deuzivan</span>!
+      </h1>
+      <h2 className="text-white text-3xl py-2 font-bold">About Me</h2>
+      <p className="text-1xl text-neutral-400 font-light">
+        I&apos;ve been passionate about programming since I was 14 years old,
+        I&apos;ve been trying my whole life to evolve and learn with each new
+        project.
+        <br />
+        You can see my projects bellow!
+      </p>
+      <h2 className="text-white text-3xl py-2 font-bold">Languages</h2>
+      <ul className="text-neutral-400">
+        <li className="text-1xl font-light">- Portuguese (Native)</li>
+        <li className="text-1xl font-light">- English (Basic)</li>
+      </ul>
+
+      <div className="flex justify-center pt-6 gap-4">
+        <Link href="#">
+          <Instagram strokeWidth={1.5} size={32} />
+        </Link>
+        <Link href="https://github.com/DeuzivanLima">
+          <Github strokeWidth={1.5} size={32} />
+        </Link>
+        <Link href="#">
+          <Linkedin strokeWidth={1.5} size={32} />
+        </Link>
+        <Link href="#">
+          <Youtube strokeWidth={1.5} size={32} />
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
--- app/components/Chat/index.js
+++ app/components/Chat/index.js
@@ -1,76 +1,79 @@
-'use client'
-import { Send } from "lucide-react"
-import { Layout } from "../Layout"
-import { NavigationBar } from "../NavigationBar"
-import { useEffect, useState } from "react"
+"use client";
+import { Send } from "lucide-react";
+import { Layout } from "../Layout";
+import { NavigationBar } from "../NavigationBar";
+import { useEffect, useState } from "react";
 
 const formatDate = (time) => {
-    let date = new Date(time)
-    return ` ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`
-}
+  let date = new Date(time);
+  return ` ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`;
+};
 
 export const Chat = () => {
-    const [messages, setMessages] = useState([])
-    const [content, setContent] = useState("")
-    const [updateChat, setUpdateChat] = useState(0)
+  const [messages, setMessages] = useState([]);
+  const [content, setContent] = useState("");
+  const [updateChat, setUpdateChat] = useState(0);
 
-    useEffect(() => {
-        const fetchMessages = async () => {
-            const res = await (await fetch("/api/dump-messages")).json()
-            setMessages(res)
-        }
-        fetchMessages()
-    }, [updateChat])
-    
-    const submitMessage = async () => {
-        if(content.length <= 64 && content.length > 0) {
-            await fetch("/api/send-message", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                cache: "no-store",
-                body: JSON.stringify({content: content})
-            })
-    
-            setUpdateChat(updateChat + 1)
-            setContent("")
-        } else {
-            alert("The max characters is 64 and not can be empty")
-        }
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const res = await (await fetch("/api/dump-messages")).json();
+      setMessages(res);
+    };
+    fetchMessages();
+  }, [updateChat]);
+
+  const submitMessage = async () => {
+    if (content.length <= 64 && content.length > 0) {
+      await fetch("/api/send-message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+        body: JSON.stringify({ content: content }),
+      });
+
+      setUpdateChat(updateChat + 1);
+      setContent("");
+    } else {
+      alert("The max characters is 64 and not can be empty");
     }
+  };
 
-    return (
-        <Layout className={"h-[90vh]"}>
-            <NavigationBar title={'Chat Me'}/>
-            <ul className="text-white m-4 flex flex-col gap-2 h-[65vh] overflow-scroll">
-                {
-                    messages.map((val) => {
-                        return (
-                            <li className="bg-neutral-900 rounded-3xl p-4 break-words" key={val.id}>
-                                <p className="text-neutral-500 text-sm">Post ID: { val.id }</p>
-                                <p>{ val.content }</p>
-                                <p className="text-right text-neutral-700 text-sm">{formatDate(val.createdAt)}</p>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+  return (
+    <Layout className={"h-[90vh]"}>
+      <NavigationBar title={"Chat Me"} />
+      <ul className="text-white m-4 flex flex-col gap-2 h-[65vh] overflow-scroll">
+        {messages.map((val) => {
+          return (
+            <li
+              className="bg-neutral-900 rounded-3xl p-4 break-words"
+              key={val.id}
+            >
+              <p className="text-neutral-500 text-sm">Post ID: {val.id}</p>
+              <p>{val.content}</p>
+              <p className="text-right text-neutral-700 text-sm">
+                {formatDate(val.createdAt)}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
 
-            <div className="flex justify-center items-center gap-2 bottom-2 p-4">
-                <input
-                    placeholder="Type here..."
-                    onChange={e => setContent(e.target.value)}
-                    value={content}
-                    className="outline-none p-4 rounded-full block w-[80%]"
-                    />
-                <button
-                    className="bg-neutral-800 p-4 rounded-full block"
-                    onClick={submitMessage}
-                    >
-                    <Send className="text-white"/>
-                </button>
-            </div>
-        </Layout>
-    )
-}
+      <div className="flex justify-center items-center gap-2 bottom-2 p-4">
+        <input
+          placeholder="Type here..."
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+          className="outline-none p-4 rounded-full block w-[80%]"
+        />
+        <button
+          className="bg-neutral-800 p-4 rounded-full block"
+          onClick={submitMessage}
+        >
+          <Send className="text-white" />
+        </button>
+      </div>
+    </Layout>
+  );
+};
