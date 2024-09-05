bb5675f43b6eef1fc221813147c3fbcca8973a54
Deuzivan <158287163+DeuzivanLima@users.noreply.github.com>
2024-09-05 12:15:54 +0000

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Home",
-  description: "Deuzivan is my name, I am a self-taught programmer and I always seek to evolve with each project. I've worked in a small agency and done real projects, always trying to meet demands."
+  description:
+    "Deuzivan is my name, I am a self-taught programmer and I always seek to evolve with each project. I've worked in a small agency and done real projects, always trying to meet demands.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
