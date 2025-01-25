87076e0c14d2cd72c098a9ce03af75e428692d98
DeuzivanViana <deuzivanlimaviana@proton.me>
2025-01-25 15:54:39 -0300

    add desktop navigation style

--- app/components/NavigationBar/index.tsx
+++ app/components/NavigationBar/index.tsx
@@ -1,36 +1,52 @@
 'use client'
-import { Menu, MessageSquareText, BadgeHelp, User, MessageSquare, Home, LogIn } from 'lucide-react'
+import { Menu, BadgeHelp, Home, XIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useState } from 'react'
 
-export const NavigationBar = ({title = "Home"}) => {
-    const [is_open_menu, setIsOpenMenu] = useState(false)
+export const NavigationBar = ({ title = "Portfolio" }) => {
+  const [is_open_menu, setIsOpenMenu] = useState(false);
 
-    return (
-        <>
-            <nav className='bg-neutral-900 p-6 shadow-md flex justify-between'>
-                <h1 className='text-white'>{title}</h1>
-                <Menu className='text-white z-50' onClick={() => setIsOpenMenu(!is_open_menu)}/>
-            </nav>
+  return (
+    <nav className="bg-neutral-900 flex items-center justify-between p-4 mx-auto">
+      <h1 className="text-white text-lg">{title}</h1>
 
-            <div className={'duration-500 bg-black w-[75%] max-w-[400px] h-[100%] fixed top-0 text-white pt-[5rem] z-10 ' +  (is_open_menu ? 'right-0' : 'right-[-100%]')}>
-                <ul>
-                    <li>
-                        <Link href={'/'} className='flex gap-4 p-4 bg-neutral-900 hover:bg-neutral-700 hover:animate-pulse border-neutral-500'>
-                            <Home/>
-                            <span>Home</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={'#'} className='flex gap-4 p-4 mt-1 bg-neutral-900 hover:bg-neutral-700 hover:animate-pulse border-neutral-500'>
-                            <BadgeHelp/>
-                            <span>About</span>
-                        </Link>
-                    </li>
-                </ul>
+      { 
+        is_open_menu ?
+          <XIcon
+            className="text-white z-50 md:hidden cursor-pointer"
+            onClick={() => setIsOpenMenu(!is_open_menu)}
+            />
+        :
+          <Menu
+            className="text-white z-50 md:hidden cursor-pointer"
+            onClick={() => setIsOpenMenu(!is_open_menu)}
+            />
+      }
 
-                <footer className='absolute bottom-6 text-center w-full text-neutral-800'>Version: 0.1.0</footer>
-            </div>
-        </>
-    )
-}
+
+      <ul
+        className={`fixed top-0 right-0 h-full w-72 bg-[#121212] p-6 pt-12 md:pt-0 z-40 transition-transform duration-300 ${is_open_menu ? 'translate-x-0' : 'translate-x-full'
+          } md:static md:flex md:items-center md:gap-4 md:h-auto md:w-auto md:p-0 md:bg-transparent md:translate-x-0`}
+      >
+        <li>
+          <Link
+            href="/"
+            className="flex items-center gap-4 p-4 text-white hover:bg-neutral-700 rounded-md md:p-2 border-b md:border-none border-neutral-400"
+          >
+            <Home className="md:hidden" />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="flex items-center gap-4 p-4 text-white hover:bg-neutral-700 rounded-md md:p-2 border-b md:border-none border-neutral-400"
+          >
+            <BadgeHelp className="md:hidden" />
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
